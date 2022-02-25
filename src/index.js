92e9eabadddd888e32dac895ab243949b49602f9
Natalia Orsi <naty.m.orsi@gmail.com>
2022-02-25 15:20:56 +0100

    Fixed the url redirection

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@
 
   function search() {
     let keyword = searchKeyword.value;
-    window.location.href = `https://www.google.com/search?q=${keyword}&sxsrf=APq-WBvN3zi75w61u2QjK3Hgl3eftQ0mvA%3A1645111667253&source=hp&ei=c2kOYtavDZX4sAfJpoX4CQ&iflsig=AHkkrS4AAAAAYg53g8tOdZUmbRcYNj3OxrVLPV0DhChS&ved=0ahUKEwiWtanEhof2AhUVPOwKHUlTAZ8Q4dUDCAY&uact=5&oq=${keyword}&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBwguENQCEEMyBAgAEEMyCgguEMcBENEDEEMyBAgAEEMyBAgAEEMyBwguENQCEEMyBwguENQCEEMyBAgAEEMyBAgAEEM6BwgjEOoCECc6BQgAEIAEOgsILhCABBDHARCjAjoICC4QgAQQ1AI6CwguEIAEEMcBENEDUP0QWKgWYJgaaAFwAHgAgAF9iAHwA5IBAzMuMpgBAKABAbABCg&sclient=gws-wiz`;;
+    location.replace(`https://www.google.com/search?q=${keyword}`);
 }
  
