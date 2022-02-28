76c4d73883b4c677a2b33086cf1584a0305867a9
Natalia Orsi <naty.m.orsi@gmail.com>
2022-02-28 15:13:07 +0100

    Taked the form out, to fix the search engine

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-  let searchKeyword = document.querySelector("#search-input");
-  searchKeyword.addEventListener("keyup", function(event){
-      if(event.code === "Enter") {
-          search();
-      }
-  });
+ let searchKeyword = document.querySelector("#search-input");
+ searchKeyword.addEventListener("keyup", function (event) {
+   if (event.code === "Enter" || event.code === "NumpadEnter") {
+     search();
+   }
+ });
 
-  function search() {
-    let keyword = searchKeyword.value;
-    location.replace(`https://www.google.com/search?q=${keyword}`);
-}
- 
+ function search() {
+   let keyword = searchKeyword.value;
+   location.replace(`https://www.google.com/search?q=${keyword}`);
+ }
